0d3ae7035da3df4fc3188e183b9b5f65da28a5a5
devamdarji22 <devamdarji22@gmail.com>
2020-11-03 18:38:40 +0530

    Few changes

--- app/src/main/java/com/jadd/quizmanager/SignUpActivity.java
+++ app/src/main/java/com/jadd/quizmanager/SignUpActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +22,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class SignUpActivity extends AppCompatActivity {
+import java.util.regex.Pattern;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText nameField,emailField,passwordField;
     String name,email,password;
@@ -42,24 +45,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         signUpButton = findViewById(R.id.sign_up_activity_button);
 
-        signUpButton.setOnClickListener(view -> {
-            name = nameField.getText().toString();
-            password = passwordField.getText().toString();
-            email = emailField.getText().toString();
-            host = new Host(email,name);
-            auth.createUserWithEmailAndPassword(host.geteMail(),password)
-                    .addOnCompleteListener(SignUpActivity.this, task -> {
-                        if(task.isSuccessful()){
-                            Toast.makeText(SignUpActivity.this, "Ok", Toast.LENGTH_SHORT).show();
-
-
-                        }
-                        else {
-                            // If sign in fails, display a message to the user.
-
-                        }
-                    });
-        });
+        signUpButton.setOnClickListener(this::onClick);
 
     }
 
@@ -69,5 +55,61 @@ public class SignUpActivity extends AppCompatActivity {
         FirebaseUser currentUser = auth.getCurrentUser();
 
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign_up_activity_button:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        name = nameField.getText().toString();
+        password = passwordField.getText().toString();
+        email = emailField.getText().toString();
+        if(name.isEmpty()){
+            nameField.setError("Name cannot be empty!");
+            nameField.requestFocus();
+            return;
+        }
+        if(email.isEmpty()){
+            emailField.setError("Email cannot be empty!");
+            emailField.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailField.setError("Please enter valid Email!");
+            emailField.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            passwordField.setError("Please enter Password!");
+            passwordField.requestFocus();
+            return;
+        }
+
+
+        Toast.makeText(this, "In onClick", Toast.LENGTH_SHORT).show();
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this, "Ok", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                        else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Sign up failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 
 }
